739171d50df605199ac2f610d772b5cd22b4a608
Quvonchbek Botirov <quvonchbekbotirov00@gmail.com>
2026-01-24 18:12:23 +0500

    Time: 858 ms (77.97%), Space: 33.5 MB (31.19%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -1,20 +1,11 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
-        # 1-qadam: Massivni o'sish tartibida joylashtiramiz
         nums.sort()
-        
-        # O'zgaruvchilarni tayyorlaymiz
-        max_sum = 0
-        n = len(nums)
-        
-        # 2-qadam: Faqat yarimigacha aylanamiz (n / 2)
-        # Chunki har safar bitta boshidan, bitta oxiridan olamiz
-        for i in range(n // 2):
-            # Eng kichik (i) + Eng katta (n - 1 - i)
-            current_sum = nums[i] + nums[n - 1 - i]
-            
-            # Agar hozirgi yig'indi oldingisidan katta bo'lsa, uni eslab qolamiz
-            max_sum = max(max_sum, current_sum)
-            
-        return max_sum
-        
+        l=0
+        r=len(nums)-1
+        maxi=0
+        while l<r:
+            maxi=max(maxi,nums[l]+nums[r])
+            l+=1
+            r-=1
+        return maxi
